337dc7dfc80e9356e02f0027b51a397773d467c9
Anton Bugrin <abugrin@yandex.ru>
2025-07-02 15:44:10 +0300

    depth

--- app/api/logger/route.ts
+++ app/api/logger/route.ts
@@ -1,5 +1,6 @@
 export const POST = async (req: Request): Promise<Response> => {
-  console.log(await req.json());
+  //console.log(await req.json());
+  console.dir(await req.json(), { depth: null });
 
   return Response.json('OK');
 };
